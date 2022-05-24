956ea506b8ef24336358830e97f78c7f04247142
Daniel Siepmann <coding@daniel-siepmann.de>
2022-05-24 16:44:17 +0200

    Refactor ssh keys
    Make public what is already known to the public.
    Also define keys in variables for easier maintenance.
    
    Some hostings still don't support ed.

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -1,7 +1,20 @@
 _:
 
-{
+let
+  oldKey = "~/.ssh/id_rsa";
+  newKey = "~/.ssh/id_ed25519";
+
+in {
   enable = true;
-  matchBlocks = import ./ssh-match-blocks.nix {
+  matchBlocks = {
+
+    "github.com" = {
+        user = "git";
+        hostname = "github.com";
+        identityFile = "${newKey}";
+    };
+
+  } // import ./ssh-match-blocks.nix {
+    inherit oldKey newKey;
   };
 }
