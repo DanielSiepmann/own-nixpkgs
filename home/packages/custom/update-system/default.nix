60188ff1da028c638854daba510757c5e1e861be
Daniel Siepmann <coding@daniel-siepmann.de>
2022-05-30 09:53:59 +0200

    Remove nix-index
    It is slow as hell and I don't think I need the benefits.

--- home/packages/custom/update-system/default.nix
+++ home/packages/custom/update-system/default.nix
@@ -8,7 +8,6 @@ writeShellApplication {
     home-manager switch
     home-manager expire-generations '-30 days'
     nix-env --delete-generations +5
-    nix-index
     oldVersion=$(home-manager generations | head -n 2 | tail -n 1 | cut -d' ' -f 7)
     newVersion=$(home-manager generations | head -n 1 | cut -d' ' -f 7)
     nvd diff "$oldVersion" "$newVersion"
--- home/programs.nix
+++ home/programs.nix
@@ -4,13 +4,6 @@
   # Let Home Manager install and manage itself.
   home-manager.enable = true;
 
-  # Add hook to zsh and bash.
-  # Does not work as there is no sqlite file.
-  # command-not-found.enable = true;
-  # Use nix-index as replacement.
-  # This needs to build index, which I do in custom-update-system
-  nix-index.enable = true;
-
   neovim = import ./programs/neovim.nix {
     inherit pkgs;
   };
