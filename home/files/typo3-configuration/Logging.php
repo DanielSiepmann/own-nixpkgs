1fcf4bba0bb07a47ce6b5aebe44a2e65d1995a59
Daniel Siepmann <coding@daniel-siepmann.de>
2022-08-16 07:41:34 +0200

    Adjust TYPO3 custom logging
    Use logFileInfix instead of logFile.
    As path changes between TYPO3 versions and logs are no longer stored
    within typo3temp.
    Instead we distinguish the custom log by file infix.

--- home/files/typo3-configuration/Logging.php
+++ home/files/typo3-configuration/Logging.php
@@ -3,7 +3,7 @@
 $GLOBALS['TYPO3_CONF_VARS']['LOG']['das']['writerConfiguration'] = [
     \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
         \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
-            'logFile' => 'typo3temp/var/log/typo3_das.log',
+            'logFileInfix' => 'das',
         ],
     ],
 ];
