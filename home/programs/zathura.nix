683684609238b60f9844f895d5c1156a4f838168
Daniel Siepmann <coding@daniel-siepmann.de>
2022-08-24 17:29:20 +0200

    Remove wrong options from zathura configuration
    These were duplicates of completion-group repeating completion.

--- home/programs/zathura.nix
+++ home/programs/zathura.nix
@@ -10,15 +10,12 @@ _:
     completion-fg = "#9cd9f0";
     completion-group-bg = "#000000";
     completion-group-fg = "#9cd9f0";
-    completion-completion-bg = "#000000";
-    completion-completion-fg = "#9cd9f0";
     inputbar-bg = "#2e3436";
     inputbar-fg = "#9cd9f0";
     statusbar-bg = "#2e3436";
     statusbar-fg = "#9cd9f0";
     render-loading-bg = "#2e3436";
     render-loading-fg = "#9cd9f0";
-
 
     window-title-basename = true;
     statusbar-basename = true;
