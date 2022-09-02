7cf0b6ed7a272f32c508cbafcbaae87712dcec6d
Daniel Siepmann <coding@daniel-siepmann.de>
2022-09-02 13:28:57 +0200

    Adjust GTK theme

--- overlays/qogir-theme/default.nix
+++ overlays/qogir-theme/default.nix
@@ -1,6 +1,9 @@
 self: super: {
   qogir-theme = super.qogir-theme.overrideAttrs(old: {
     patches = [
+      # TODO: Find out how to properly render inactive menu items
+      # TODO: Find out how to properly render background of selected text
+      # TODO: Properly adjust svgs and then compile them
       ./colors.patch
     ];
   });
