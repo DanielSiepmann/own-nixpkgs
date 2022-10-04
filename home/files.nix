285a366357acca1da6a8589b24cfc39beef353ec
Daniel Siepmann <coding@daniel-siepmann.de>
2022-10-04 15:09:44 +0200

    Maintain docker config.json which includes credentials

--- home/files.nix
+++ home/files.nix
@@ -5,4 +5,5 @@ _:
   ".profile".source = ./files/profile;
   ".myclirc".source = ./files/myclirc;
   ".agignore".source = ./files/agignore;
+  ".docker/config.json".source = ./files/docker/config.json;
 }
