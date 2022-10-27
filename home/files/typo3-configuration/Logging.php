7d6af0c5cea4be8714a90430df30fab0232cd2b8
Daniel Siepmann <coding@daniel-siepmann.de>
2022-10-27 14:25:44 +0200

    Add custom TYPO3 logging processor to add URIs to log entries

--- home/files/typo3-configuration/Logging.php
+++ home/files/typo3-configuration/Logging.php
@@ -1,5 +1,23 @@
 <?php
 
+namespace Codappix\CdxLogging;
+
+use TYPO3\CMS\Core\Log\LogRecord;
+use TYPO3\CMS\Core\Log\Processor\AbstractProcessor;
+use TYPO3\CMS\Core\Log\Processor\ProcessorInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class UriProcessor extends AbstractProcessor implements ProcessorInterface
+{
+    public function processLogRecord(LogRecord $logRecord)
+    {
+        $logRecord->addData([
+            'URI' => GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL'),
+        ]);
+        return $logRecord;
+    }
+}
+
 $GLOBALS['TYPO3_CONF_VARS']['LOG']['das']['writerConfiguration'] = [
     \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
         \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
