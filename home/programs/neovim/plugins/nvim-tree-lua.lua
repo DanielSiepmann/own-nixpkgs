87f3310ae39a35454c577fc48b178eb2abef0ad7
Daniel Siepmann <coding@daniel-siepmann.de>
2022-10-27 14:39:38 +0200

    Keep netrw as it provides features like gx to open urls and files

--- home/programs/neovim/plugins/nvim-tree-lua.lua
+++ home/programs/neovim/plugins/nvim-tree-lua.lua
@@ -1,7 +1,3 @@
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- empty setup using defaults
 require("nvim-tree").setup({
     sync_root_with_cwd = false,
