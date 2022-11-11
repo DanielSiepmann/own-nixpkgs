b6f52946e5de3fd96aa389ca8e049b87ccd8d80a
Daniel Siepmann <coding@daniel-siepmann.de>
2022-11-11 14:08:42 +0100

    Once more try to include nvd in system update

--- home/packages/custom/update-system/default.nix
+++ home/packages/custom/update-system/default.nix
@@ -3,14 +3,14 @@
 writeShellApplication {
   name = "custom-update-system";
 
-  # Including this doesn't work
-  # oldVersion=$(home-manager generations | head -n 2 | tail -n 1 | cut -d' ' -f 7)
-  # newVersion=$(home-manager generations | head -n 1 | cut -d' ' -f 7)
-  # nvd diff "$oldVersion" "$newVersion"
-
   text = ''
     nix-channel --update
     home-manager switch
+
+    oldVersion=$(home-manager generations | head -n 2 | tail -n 1 | cut -d' ' -f 7)
+    newVersion=$(home-manager generations | head -n 1 | cut -d' ' -f 7)
+    nvd diff "$oldVersion" "$newVersion"
+
     home-manager expire-generations '-30 days'
     nix-env --delete-generations +5
     nix store gc
