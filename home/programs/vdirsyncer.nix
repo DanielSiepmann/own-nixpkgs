db9101cdc10e70e020be4d0a2495f1e74fd8610a
Daniel Siepmann <coding@daniel-siepmann.de>
2022-12-08 09:10:28 +0100

    Add holiday calendar
    Also fix typo

--- home/programs/vdirsyncer.nix
+++ home/programs/vdirsyncer.nix
@@ -40,6 +40,19 @@
     type = "http"
     url = "https://calendar.google.com/calendar/ical/b9o52fobqjak8oq8lfkhg3t0qg%40group.calendar.google.com/public/basic.ics"
 
+    [pair holidays]
+    a = "holidays_calendar_private"
+    b = "holidays_calendar_public"
+    collections = null
+    [storage holidays_calendar_private]
+    type = "caldav"
+    url = "https://nextcloud.daniel-siepmann.de/remote.php/dav/calendars/dsiepmann/feiertage-synced/"
+    username = "dsiepmann"
+    password.fetch = ["command", "${pkgs.libsecret}/bin/secret-tool", "lookup", "account", "app-dsiepmann@nextcloud.daniel-siepmann.de"]
+    [storage holidays_calendar_public]
+    type = "http"
+    url = "https://feiertage.ics.tools/nordrhein-westfalen.ics"
+
     [pair company_calendar]
     a = "company_calendar_local"
     b = "company_calendar_remote"
@@ -52,7 +65,7 @@
     type = "caldav"
     url = "https://nextcloud.codappix.com/remote.php/dav"
     username = "daniel.siepmann"
-    password.fetch = ["command", "${pkgs.libsecret}/bin/secret-tool", "lookup", "account", "app-dsiepmann@nextcloud.codapix.com"]
+    password.fetch = ["command", "${pkgs.libsecret}/bin/secret-tool", "lookup", "account", "app-dsiepmann@nextcloud.codappix.com"]
     start_date = "datetime.now() - timedelta(days=60)"
     end_date = "datetime.now() + timedelta(days=365)"
 
@@ -64,7 +77,7 @@
     type = "caldav"
     url = "https://nextcloud.codappix.com/remote.php/dav/calendars/daniel.siepmann/sac-scrum-team-synced/"
     username = "daniel.siepmann"
-    password.fetch = ["command", "${pkgs.libsecret}/bin/secret-tool", "lookup", "account", "app-dsiepmann@nextcloud.codapix.com"]
+    password.fetch = ["command", "${pkgs.libsecret}/bin/secret-tool", "lookup", "account", "app-dsiepmann@nextcloud.codappix.com"]
     [storage sac_team_calendar_public]
     type = "http"
     url = "https://calendar.google.com/calendar/ical/0ks410dho8f5ohcik361i4c2bs%40group.calendar.google.com/public/basic.ics"
