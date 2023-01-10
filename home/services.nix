c299c3d0ef11c39a3c943eb60f68f228d53b3c5f
Daniel Siepmann <coding@daniel-siepmann.de>
2023-01-10 15:27:04 +0100

    Add network manager applet via option
    Do not manually ad the manager

--- home/services.nix
+++ home/services.nix
@@ -9,6 +9,8 @@
     gpg-agent = import ./services/gpg-agent.nix {
     };
 
+    network-manager-applet.enable = true;
+
     languagetool.enable = true;
     mailhog.enable = true;
 
--- home/windowManager/i3.nix
+++ home/windowManager/i3.nix
@@ -177,14 +177,6 @@ in {
       ];
     };
 
-    startup = [
-      # Start programs / applets
-      {
-        command = "nm-applet";
-        notification = false;
-      }
-    ];
-
     keybindings = {
       # kill focused window
       "${modifier}+Shift+q" = "kill";
