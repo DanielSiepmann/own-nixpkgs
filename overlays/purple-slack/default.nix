6cae4350cb2fccc55dc33cc5e0d60a7532ac8b72
Daniel Siepmann <coding@daniel-siepmann.de>
2023-01-25 07:50:18 +0100

    Update slack libpurple

--- overlays/purple-slack/default.nix
+++ overlays/purple-slack/default.nix
@@ -1,19 +1,21 @@
 self: super: {
   purple-slack = super.purple-slack.overrideAttrs(old: {
-    version = "2022-08-30";
+    version = "2023-01-19";
 
     src = super.fetchFromGitHub {
       owner = "dylex";
       repo = "slack-libpurple";
-      rev = "f91a5db6f148668afac8384eb349c545308b14fd";
-      sha256 = "sha256-VrkjeE8MYyrjdhmFS4f1OZDN6v69lDKITQQUCj3/1zY=";
+      rev = "6778b7c7ae28d9b62339262245c8ca7d11fb5a16";
+      sha256 = "sha256-smv2/bHLD5OJyUZhvgzqqzYO6NVWAY6d0dLamiXIEm4=";
     };
 
     # Further ideas to add:
     # Adjust color of text rendered to start call or open chat
     # Add command to react to messages
+    #   https://github.com/dylex/slack-libpurple/issues/155
     # Support sending of files
     # Prevent duplicates of incoming messages (happens from time to time)
+    #   https://github.com/dylex/slack-libpurple/issues/155
     # Formatting (bold, italic, …) see: https://github.com/dylex/slack-libpurple/pull/122
     # Receive reactions
     patches = (old.patches or []) ++ [
