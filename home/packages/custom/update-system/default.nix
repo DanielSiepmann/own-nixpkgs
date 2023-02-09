e3648c17cfae3a8394dd785ecf88500b69e7ac1d
Daniel Siepmann <coding@daniel-siepmann.de>
2023-02-09 12:56:10 +0100

    WIP|Improve update
    Goal: Show what will change before applying.
    Should then also be used for nixos update.
    
    oldVersion line breaks for some reason?

--- home/packages/custom/update-system/default.nix
+++ home/packages/custom/update-system/default.nix
@@ -5,11 +5,21 @@ writeShellApplication {
 
   text = ''
     nix-channel --update
-    home-manager switch
+    home-manager build
 
+    echo "fetch old version"
     oldVersion=$(home-manager generations | head -n 2 | tail -n 1 | cut -d' ' -f 7)
-    newVersion=$(home-manager generations | head -n 1 | cut -d' ' -f 7)
-    nvd diff "$oldVersion" "$newVersion"
+    nvd diff "$oldVersion" ./result
+    rm ./result
+
+    read -p "Continue with update? [Yy]" -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        exit 1
+    fi
+
+    home-manager switch
 
     home-manager expire-generations '-30 days'
     nix-env --delete-generations +5
