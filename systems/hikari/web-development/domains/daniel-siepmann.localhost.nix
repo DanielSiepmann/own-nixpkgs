3c66511ff3a96cc63bd9e2dec04321402bfad252
Daniel Siepmann <coding@daniel-siepmann.de>
2023-02-18 11:23:29 +0100

    Follow naming conventions also for own projects

--- systems/hikari/web-development/domains/daniel-siepmann.localhost.nix
+++ systems/hikari/web-development/domains/daniel-siepmann.localhost.nix
@@ -3,7 +3,7 @@
 import ./../lib/create-typo3.nix {
   inherit config lib pkgs;
 
-  domain = "daniel-siepmann.localhost";
+  domain = "daniel-siepmann.own.localhost";
   relativeDocumentRoot = "own/daniel-siepmann.de/project/public/";
   databaseName = "own_danielsiepmann";
   phpPackage = pkgs.php82;
